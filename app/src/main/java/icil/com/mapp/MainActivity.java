0ebef4384a9619b361770609ec7e445a36a85e6b
pchan1401-ICIL <pchan1401@hanmail.net>
2016-06-11 11:50:10 +0900

    Camera2 API preview

---
+++ app/src/main/java/icil/com/mapp/MainActivity.java
@@ -0,0 +1,66 @@
+package icil.com.mapp;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.TextureView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextureView mCameraTextureView;
+    private Preview mPreview;
+
+    Activity mainActivity = this;
+
+    private static final String TAG = "MAINACTIVITY";
+
+    static final int REQUEST_CAMERA = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCameraTextureView = (TextureView) findViewById(R.id.cameraTextureView);
+        mPreview = new Preview(this, mCameraTextureView);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CAMERA:
+                for (int i = 0; i < permissions.length; i++) {
+                    String permission = permissions[i];
+                    int grantResult = grantResults[i];
+                    if (permission.equals(Manifest.permission.CAMERA)) {
+                        if(grantResult == PackageManager.PERMISSION_GRANTED) {
+                            mCameraTextureView = (TextureView) findViewById(R.id.cameraTextureView);
+                            mPreview = new Preview(mainActivity, mCameraTextureView);
+                            Log.d(TAG,"mPreview set");
+                        } else {
+                            Toast.makeText(this,"Should have camera permission to run", Toast.LENGTH_LONG).show();
+                            finish();
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mPreview.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPreview.onPause();
+    }
+}
